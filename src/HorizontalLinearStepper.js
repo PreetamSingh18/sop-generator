4b6f009e8a8db85aa16c385548f1dedfa55e726f
Preetam Singh <spreetam2001@gmail.com>
2023-09-04 14:05:56 +0530

    Responsive

--- src/HorizontalLinearStepper.js
+++ src/HorizontalLinearStepper.js
@@ -133,7 +133,8 @@ export default function HorizontalLinearStepper() {
       {activeStep === steps.length ? (
         <React.Fragment>
           <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
+            All steps completed - your response saved and sent to your registered email id.
+            
           </Typography>
           <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
             <Box sx={{ flex: '1 1 auto' }} />
